48bd03e1caaf50b65bc860a266b236eb60a84d0b
brackium-investments <brackium4@gmail.com>
2025-11-20 00:54:55 +0100

    active

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
 
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
